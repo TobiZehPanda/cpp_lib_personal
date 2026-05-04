a8e333943b939476f9a9b049ca58f24f0e82f77c
Tobi Sakatoi <tobizehpanda@gmail.com>
2026-05-04 16:25:19 -0500

    lib

---
+++ include/cpp_lib_personal/main.hpp
@@ -0,0 +1,13 @@
+#ifndef CPP_LIB_PERSONAL
+#define CPP_LIB_PERSONAL
+
+#include <string>
+#include <vector>
+#include <filesystem>
+
+std::vector<std::string> split(std::string& s, const std::string& delimiter);
+bool replace(std::string& str, const std::string& from, const std::string& to);
+std::filesystem::path replace_home(std::filesystem::path str);
+std::filesystem::path revert_home(std::string& str);
+
+#endif
